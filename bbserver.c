76917bb4cd5686fbd8a002a3ed79e79ccfddf1fa
Jeremy <jpb42@students.uwf.edu>
2018-02-15 18:45:12 -0600

    Print out addrinfo

--- bbserver.c
+++ bbserver.c
@@ -17,25 +17,52 @@ int main(int argc, char **argv)
 {
 	createServer();
 
+	printf("Program completed\n");
+
 	return 0;
 }
 
 int createServer()
 {
 	int status;
-	struct addrinfo hints, *serverInfo;
+	struct addrinfo hints, *serverInfo,
+					*p;
+	char ipstr[INET6_ADDRSTRLEN];
+
 
 	memset(&hints, 0, sizeof hints); // make sure the struct is empty
 	hints.ai_family = AF_UNSPEC;     // IPv4 or IPv6
 	hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
 	hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
 
-	if ((status = getaddrinfo(NULL, "3490", &hints, &serverInfo)) != 0) {
+	if ((status = getaddrinfo(NULL, "6969", &hints, &serverInfo)) != 0) {
 		fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
 		exit(1);
 	}
 
-	printf("Program completed\n");
+
+	for (p = serverInfo; p != NULL; p = p->ai_next)
+	{
+		void *addr;
+		char *ipver;
+
+		if (p->ai_family == AF_INET) 
+		{
+			struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+			addr = &(ipv4->sin_addr);
+			ipver = "IPv4";
+		}
+		else 
+		{ 
+			struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+			addr = &(ipv6->sin6_addr);
+			ipver = "IPv6";
+		}
+
+		// convert the IP to a string and print it:
+		inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+		printf("  %s: %s\n", ipver, ipstr);
+	}
 
 	return status;
 }
