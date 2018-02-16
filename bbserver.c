82de4730089716ba4d0c0a0c19bb3634212541c4
Jeremy <jpb42@students.uwf.edu>
2018-02-15 18:32:26 -0600

    Renamed results struct.

--- bbserver.c
+++ bbserver.c
@@ -8,6 +8,9 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+// Project
+#include <bbserver.h>
+
 // Defines
 
 int main(int argc, char **argv) 
@@ -20,14 +23,14 @@ int main(int argc, char **argv)
 int createServer()
 {
 	int status;
-	struct addrinfo hints, *results;
+	struct addrinfo hints, *serverInfo;
 
 	memset(&hints, 0, sizeof hints); // make sure the struct is empty
 	hints.ai_family = AF_UNSPEC;     // IPv4 or IPv6
 	hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
 	hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
 
-	if ((status = getaddrinfo(NULL, "3490", &hints, &servinfo)) != 0) {
+	if ((status = getaddrinfo(NULL, "3490", &hints, &serverInfo)) != 0) {
 		fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
 		exit(1);
 	}
