667462af6e140f5933a6e7959971868c3119139a
Jeremy <jpb42@students.uwf.edu>
2018-02-17 19:33:15 -0600

    Initial commit of bbpeer

---
+++ bbpeer.c
@@ -0,0 +1,21 @@
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+// Project
+#include "bbpeer.h"
+
+// Defines
+#define ERROR -1
+
+int main(int argc, char **argv) 
+{
+	
+	return 0;
+}
---
+++ bbpeer.h
@@ -0,0 +1,7 @@
+#ifndef BBPEER_H_   
+#define BBPEER_H_
+
+#include <netinet/in.h>
+
+
+#endif // BBPEER_H_
