a05df02fc4a9bfd0b1f7d286f66824ec8f7dc37b
Jeremy <jpb42@students.uwf.edu>
2018-04-21 19:40:29 -0500

    lol

--- bbpeer.h
+++ bbpeer.h
@@ -13,6 +13,6 @@ int createClientSocket(char *hostName, int port, struct sockaddr_in *dest);
 
 int bindClientSocket(int sockfd, int port);
 
-void requestpeer(int sockfd, struct sockaddr_in *peer, const struct sockaddr *server);
+void requestpeer(int sockfd, const struct sockaddr *server);
 
 #endif // BBPEER_H_
