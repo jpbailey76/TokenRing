13cfe6639505d848fd042e5a8bb58e5431178797
Jeremy <jpb42@students.uwf.edu>
2018-04-21 19:22:56 -0500

    need a semi

--- bbpeer.h
+++ bbpeer.h
@@ -3,16 +3,16 @@
 
 #include <netinet/in.h>
 
-typedef struct ClientData {
+typedef struct ClientData 
+{
     struct sockaddr_in client;
     struct sockaddr_in peer;
 } ClientData;
-
 
 int createClientSocket(char *hostName, int port, struct sockaddr_in *dest);
 
 int bindClientSocket(int sockfd, int port);
 
-void requestpeer(struct sockaddr_in *peer, const struct sockaddr *server)
+void requestpeer(struct sockaddr_in *peer, const struct sockaddr *server);
 
 #endif // BBPEER_H_
