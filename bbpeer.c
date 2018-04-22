9617c2f30a39f89d8ad286cbf7b5029f99d10cf3
Jeremy <jpb42@students.uwf.edu>
2018-04-22 12:33:53 -0500

    use int ptr

--- bbpeer.c
+++ bbpeer.c
@@ -245,7 +245,7 @@ void * tokenPassing_Thread(void *_sockfd)
 {
   ClientData peer;
   ssize_t len;
-  int sockfd = (int)_sockfd;
+  int sockfd = (intptr_t)_sockfd;
 
   // Pass the token around 
   sendto(sockfd, &TOKEN, sizeof TOKEN, 0,
