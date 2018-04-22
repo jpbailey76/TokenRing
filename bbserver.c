64f8e6f2931aeed4daf29ceab4dd133dd7aa194b
Jeremy <jpb42@students.uwf.edu>
2018-04-22 15:27:13 -0500

    dot

--- bbserver.c
+++ bbserver.c
@@ -39,7 +39,7 @@ int main(int argc, char **argv)
   PeerT *peerArray = new_parray(&server);
 
   printf(BLUE"DEBUG: "RESET
-  			"Client address = [%s] Peer address = [%s]\n", inet_ntoa(peerArray->client->sin_addr), inet_ntoa(peerArray->peer->sin_addr));
+  			"Client address = [%s] Peer address = [%s]\n", inet_ntoa(peerArray->client.sin_addr), inet_ntoa(peerArray->peer.sin_addr));
 
 	runServer(sockfd, peerArray, &server);
 
