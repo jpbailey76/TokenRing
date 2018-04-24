3327820cbf6889c473c4c1528776a6c0a9ef6d10
Jeremy <jpb42@students.uwf.edu>
2018-04-24 15:52:27 -0500

    lol

--- bbpeer.c
+++ bbpeer.c
@@ -332,8 +332,7 @@ void * tokenPassing_Thread(void *arg)
   // Leave the ring.
   printf(YELLOW"You've been disconnected from the ring.\n"RESET);
   sendto(sockfd, &ring, sizeof ring, 0,(struct sockaddr *) &ring.peer, sizeof ring.peer);
-  // pthread_exit(EXIT_SUCCESS);
-  // return NULL;
+  pthread_exit(EXIT_SUCCESS);
 }
 
 void peerExit(ClientData *_request)
