3cb8c4bc95ffb03d2db85553750c7c3f257ab75c
Jeremy <jpb42@students.uwf.edu>
2018-04-25 11:20:22 -0500

    Function documentation

--- bbpeer.h
+++ bbpeer.h
@@ -4,7 +4,8 @@
 #include <netinet/in.h>
 
 /**
- * 
+ *	Structure containing the clients info,
+*	as well as it's neighbors info.
  */
 typedef struct TokenRing 
 {
@@ -13,96 +14,110 @@ typedef struct TokenRing
 } TokenRing;
 
 /**
- * [verifyInput description]
- * @param argc [description]
- * @param argv [description]
+ *	Checks the arguments that the program was ran with.
+ *	If it's valid we continue, if not we throw an error.
+ *
+ * @param argc - Number of arguments passed in.
+ * @param argv - Array containing the arguments.
  */
 void verifyInput(int argc, char **argv);
 
 /**
- * [getServerInfo description]
- * @param  addr [description]
- * @param  port [description]
- * @return      [description]
+ *	Given an address and port, this function returns the addrinfo 
+ *	associated with that IP.
+ *
+ *	@param  ipAddress	- The IP Address.	
+ *	@param  port			- The port.	
+ *	@return addrinfo     - Address information for the given ipAddress:port combo.
  */
-struct addrinfo* getServerInfo(const char *addr, const char *port);
+struct addrinfo* getServerInfo(const char *ipAddress, const char *port);
 
 /**
- * [bindClientSocket description]
- * @param  sockfd [description]
- * @param  port   [description]
- * @return        [description]
+ *	Sends a 'connect' message to the server and requests for neighbor.
+ *
+ *	@param server - structure containing the server address, port, and address family.
  */
-int bindClientSocket(int sockfd, int port);
+void waitForNeighbor(const struct sockaddr *server);
 
 /**
- * [requestPeer description]
- * @param server [description]
- */
-void requestPeer(const struct sockaddr *server);
-
-/**
- * [handshake description]
+ *	Loops through the ring and sends peer addresses to all peers.
+ *	Then determines which peer gets the token first by finding the peer
+ *	with the lowest address:port combo.
  */
 void handshake();
 
 /**
- * Compares two IP addresses 
- * @param  left  [description]
- * @param  right [description]
- * @return       [description]
+ *	Given two addresses this function compares them and returns how they compare
+ *	to one another.
+ *
+ *	@param  firstPeer  - Peer to compare.
+ *	@param  secondPeer - Peer to compare.
+ *	@return -1	 - firstPeer has the lower address:port.
+ *			 0	 - Both peers have the same address:port.
+ *			 1	 - secondPeer has the lower address:port.
  */
 int compare(struct sockaddr_in *firstPeer, struct sockaddr_in *secondPeer);
 
 /**
- * [tokenPassing_Thread description]
- * @param  arg [description]
- * @return     [description]
+ *	Threaded function that runs the event loop of the token being passed around.
+ *	While still connected, peers of the token ring will pass the TOKEN to their neighbor continuously.
+ *	If a peer decides to leave the token ring they will send their TokenRing struct as a leave request.
  */
 void * tokenPassing_Thread(void *arg);
 
 /**
- * [peerExit description]
- * @param request [description]
+ *	This function takes a TokenRing member, swaps their position in the ring, and passes the token on.
+ *
+ *	@param request - Contains the peer leaving and their neighbor.
  */
 void peerExit(TokenRing *request);
 
 /**
- * [displayMenu description]
+ *	Displays the menu which contains the options available to users.
  */
 void displayMenu();
 
 /**
- * [writeToBulletin description]
- * @return [description]
+ *	Displays the menu options for writing to the BULLETIN_BOARD, takes the user's input as a message
+ *	and stores their message into the BULLETIN_BOARD (defined by arg[3])
+ *
+ *	@return SUCCESS - File was successfully opened and the message was added to the board.
+ *			ERROR	- File failed to open.
  */
 int writeToBulletin();
 
 /**
- * [getNumMessages description]
- * @return [description]
+ *	Parses the BULLETIN_BOARD and counts the number of messages it contains.
+ *
+ *	@return The number of messages in the BULLETIN_BOARD (defined by arg[3])
  */
 int getNumMessages();
 
 /**
- * [readFromBulletin description]
- * @return [description]
+ *	Displays the menu options for reading from the BULLETIN_BOARD, takes the user's input
+ *	on which message number to display and then displays that message.
+ *
+ *	@return SUCCESS - File was successfully opened and the message displayed.
+ *			ERROR	- File failed to open.
  */
 int readFromBulletin();
 
 /**
- * [printAllFromBulletin description]
- * @return [description]
+ *	Displays all the messages that are contained in the BULLETIN_BOARD.
+ *
+ *	@return SUCCESS - File was successfully opened and the messages were displayed.
+ *			ERROR	- File failed to open.
  */
 int printAllFromBulletin();
 
 /**
- * [exitRing description]
+ *	Waits for the peer to receive the token and then sets it's connection status to false,
+ *	which in turn causes the token passing thread to die.
  */
 void exitRing();
 
 /**
- * [cleanup description]
+ *	Cleans up any data structures used.
  */
 void cleanup();
 
